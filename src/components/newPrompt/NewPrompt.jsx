58a7689a77496cefee2e65b5a8bc054e3afed428
Abhay Kaushik <abhaykaushik57@gmail.com>
2024-10-28 16:26:08 +0530

    added image upload

--- src/components/newPrompt/NewPrompt.jsx
+++ src/components/newPrompt/NewPrompt.jsx
@@ -1,7 +1,16 @@
-import { useEffect, useRef } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import './newPrompt.css'
+import Upload from '../upload/Upload';
+import { IKImage } from 'imagekitio-react';
 
 const NewPrompt = () => {
+
+    const [img, setImg] = useState({
+        isLoading: false,
+        error: "",
+        dbData: {}
+    })
+
     const endRef = useRef(null);
 
     useEffect(() => {
@@ -11,12 +20,22 @@ const NewPrompt = () => {
     return (
         <div className="newPrompt">
             {/* ADD NEW CHAT */}
+
+            {img.isLoading && (
+                <div>Loading...</div>
+            )}
+
+            {/* if dbData and filePath exist */}
+            {img.dbData?.filePath && (
+                <IKImage
+                    urlEndpoint={import.meta.env.VITE_IMAGE_KIT_ENDPOINT}
+                    path={img.dbData?.filePath}
+                />
+            )} 
             <div className="endChat" ref={endRef}></div>
 
             <form className='newForm'>
-                <label htmlFor="file">
-                    <img src="/attachment.png" alt="" />
-                </label>
+                <Upload setImg={setImg}/>
                 <input id="file" type="file" multiple={false} hidden/>
                 <input type="text" placeholder='Ask me anything...' />
                 <button>
